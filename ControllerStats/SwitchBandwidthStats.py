b65222778b7fd3745e5bf1db1ec52d5d625c1325
Khayam Gondal <khayam.gondal@gmail.com>
2017-04-17 18:27:53 +0500

    Get bandwidth from controller2

--- ControllerStats/SwitchBandwidthStats.py
+++ ControllerStats/SwitchBandwidthStats.py
@@ -15,7 +15,6 @@ class SwitchBandwidthStats(RestCore):
 
     def rest_call(self, data, dpid, s_port, action):
         path = 'wm/statistics/bandwidth/'+dpid+'/'+s_port+'/json' #next use %
-	print path
         headers = {
             'Content-type': 'application/json',
             'Accept': 'application/json',
--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import ConfigParser as cp
-#import spur
 import time
+import json
 from SshHelper.SshHelper import run_command, get_cpu_usage, get_net_usage, get_hostname
 from ControllerStats.SwitchStats import SwitchStats
 from ControllerStats.SwitchBandwidthStats import SwitchBandwidthStats
@@ -15,17 +15,19 @@ nodes = parser.get('NODES', 'nodes').split(',')
 passwd = parser.get('NODES', 'pass')
 key = parser.get('NODES', 'key')
 nic = parser.get('NODES', 'nic')
-
 controller_ip = parser.get('CONTROLLER', 'ip')
 controller_port = parser.get('CONTROLLER', 'port')
 
-#switchStats = SwitchStats(controller_ip, controller_port)
-#print (switchStats.get(None))
 
+#Get the controller stats
+switchStats = SwitchStats(controller_ip, controller_port).get(None)
 switchBandwidthStats = SwitchBandwidthStats(controller_ip, controller_port)
-print (switchBandwidthStats.get(None, '00:00:7a:b1:ab:71:38:4a', '1'))
 
-#Get the switch stats
+for i in xrange(len(switchStats)):
+  switch_bw = switchBandwidthStats.get(None, switchStats[i]['switchDPID'], '1')
+  print (switch_bw[0]['bits-per-second-rx'])
+  print (switch_bw[0]['bits-per-second-tx'])
+
 # Now we will do SSH into each node and get the stats. 
 while (1):
   print time.time()
@@ -33,14 +35,14 @@ while (1):
     status, hostname = run_command(user, node, "hostname")
     status, cpu = run_command(user, node, "top -bn 1 | grep agent")
     #status, network = run_command(user, node, "netstat -tnp | grep agent")
-    status, network_recv = run_command(user, node, "cat /sys/class/net/" +nic+
-    "/statistics/rx_bytes")
-    status, network_send = run_command(user, node, "cat /sys/class/net/" +nic+
-    "/statistics/tx_bytes")
+    #status, network_recv = run_command(user, node, "cat /sys/class/net/" +nic+
+    #"/statistics/rx_bytes")
+    #status, network_send = run_command(user, node, "cat /sys/class/net/" +nic+
+    #"/statistics/tx_bytes")
   
     cpu = get_cpu_usage(cpu)
-    recv = get_net_usage(network_recv)
-    send = get_net_usage(network_send)
+    #recv = get_net_usage(network_recv)
+    #send = get_net_usage(network_send)
     hostname = get_hostname(hostname)
-    print hostname, cpu, recv, send
+    print hostname, cpu
 
